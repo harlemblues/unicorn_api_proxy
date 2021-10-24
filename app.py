0b2496a908056e400a06b31818ba29e47a305709
Unicorn Rentals <elasticvolcano@gmail.com>
2021-10-24 15:00:25 +0000

    Dude this is webscale

--- app.py
+++ app.py
@@ -10,13 +10,8 @@ Probably better way to do this, but for now this will do.
 """
 
 __author__ = 'Inigo Montoya (inigo.montoya@unicornrentals.click)'
-<<<<<<< HEAD
-__vcs_id__ = 'a0fc30732c5325293aa034c06ee55275'
-__version__ = '336d199075b9032ebfc4caf15fd4d843'
-=======
-__vcs_id__ = 'a0e0f49c0cf37866231f77ed269e514b'
-__version__ = '1ac63b8730422a88cdf86aafec9e80ad'
->>>>>>> parent of 74a05d5 (Erratic subject/verb replacements)
+__vcs_id__ = 'bf28a142d35c79adc3b780ddd2f2b13e'
+__version__ = 'b32d8a8b710d82a66a419944f82bf68d'
 
 from flask import Flask, request
 from flask_restful import Resource, Api
@@ -88,7 +83,7 @@ class Unicorns(Resource):
         #This is how our teams performance is measured - if this doent work we will not have a job long
         shared_secret = get_secret()
         headers = {'x-unicorn-api-secret': shared_secret}
-        req = requests.get(BACKEND_API+'/unicorns/'+unicorn_id, headers=headers)
+        req = requests.get(BACKEND_API+'/unicorn/'+unicorn_id, headers=headers)
         return json.loads(req.text), req.status_code, {'x-unicorn-api-secret': shared_secret}
 
     def post(self, unicorn_id):
@@ -96,13 +91,13 @@ class Unicorns(Resource):
         #Also needs a "teamid"
         #API secured by secrets the AWS unicorns have
         data = request.get_json()
-        req = requests.post(BACKEND_API+'/unicorns/'+unicorn_id, json={'snack':data['snack'],'teamid':data['teamid']})
+        req = requests.post(BACKEND_API+'/unicorn/'+unicorn_id, json={'snack':data['snack'],'teamid':data['teamid']})
         return req.json(), req.status_code
 
 
 api.add_resource(HealthCheck,'/healthcheck','/')
 api.add_resource(Unicorn, '/unicorn')
-api.add_resource(Unicorns, '/unicorns/<string:unicorn_id>')
+api.add_resource(Unicorns, '/unicorn/<string:unicorn_id>')
 
 
 if __name__ == '__main__':
